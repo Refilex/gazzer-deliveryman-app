07f0cf9115063894dd42164e4a40312ba62a36b4
ahmedkamal22 <ak0850000@gmail.com>
2024-07-13 19:38:07 +0300

    First commit in delivery man app

---
+++ lib/feature/cash_in_hand/domain/repositories/cash_in_hand_repository_interface.dart
@@ -0,0 +1,8 @@
+import 'package:stackfood_multivendor_driver/interface/repository_interface.dart';
+
+abstract class CashInHandRepositoryInterface implements RepositoryInterface {
+  Future<dynamic> makeCollectCashPayment(
+      double amount, String paymentGatewayName);
+
+  Future<dynamic> makeWalletAdjustment();
+}
---
+++ lib/feature/notification/domain/services/notification_service_interface.dart
@@ -0,0 +1,9 @@
+abstract class NotificationServiceInterface {
+  Future<dynamic> getNotificationList();
+
+  void saveSeenNotificationCount(int count);
+
+  int? getSeenNotificationCount();
+
+  Future<dynamic> sendDeliveredNotification(int? orderID);
+}
